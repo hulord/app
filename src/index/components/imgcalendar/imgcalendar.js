249ca97411018e1b5c78456228025ce7dc81ca6b
os.hupsh <os.hupsh@ffcs.cn>
2018-10-26 17:34:05 +0800

    1.页面修改class

--- src/index/components/imgcalendar/imgcalendar.js
+++ src/index/components/imgcalendar/imgcalendar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactDOM from 'react-dom'
 import moment from 'moment';
 import {Col,Row} from 'antd';
 import 'moment/locale/zh-cn';
@@ -15,12 +16,54 @@ moment.locale('zh-cn');
  *              4.翻到前一周
  */
 class ImgCalendar extends React.Component {
-  state={
-    ArrayData:[]
-  }
   constructor(props){
     super( props )
-    this.state={};
+    this.state = {
+      width: props.width || -1,
+      height: props.height || -1,
+      ArrayData:[],
+      hover:false
+    }
+  }
+  //组件渲染后调用
+  componentDidMount() {
+    this.updateSize();
+    this.getimgdate();
+    window.addEventListener('resize', () => this.updateSize());
+  }
+  //组件渲染前
+  componentWillUnmount() {
+    window.removeEventListener('resize', () => this.updateSize());
+  }
+
+  updateSize() {
+    try {
+      const parentDom = ReactDOM.findDOMNode(this).parentNode;
+      let { width, height } = this.props;
+      //如果props没有指定height和width就自适应
+      if (!width) {
+        width = parentDom.offsetWidth;
+      }
+      if (!height) {
+        height = width *0.38;
+      }
+      this.setState({ width, height });
+    } catch (ignore) {
+    }
+  }
+  //鼠标悬停事件
+  onMouseOver = (id) =>{
+    let obj = ReactDOM.findDOMNode(this[`btn-${id}`]);
+    obj.classSet = "hover";
+  }
+  //鼠标移出事件
+   onMouseOut = (id) =>{
+    let obj = ReactDOM.findDOMNode(this[`btn-${id}`]);
+    obj.style.backgroundColor = "";
+  }
+  //鼠标点击事件
+  onClick = () =>{
+
   }
   getimgdate = ()=>{
     //获取之前14天的记录
@@ -30,27 +73,31 @@ class ImgCalendar extends React.Component {
       ArrayData.push(moment().subtract('days',i).date());
     }
     ArrayData.sort(function(a,b){return a-b;})
-    return (
-      <Col span={24} className="wells">
-        <Col span={6}>back</Col>
-        {
-          ArrayData.map(item => (  
-            <Col className="Position_R" style={{height:"300px",backgroundImage:`url(${img})`,backgroundSize:"100% 100%"}} span={6} key={item}>
-              <div className="ShadeWell">{item}</div>
-            </Col>
-          ))
-        }
-        <Col span={6}>next</Col>
-      </Col>
-    );
+    this.setState({ArrayData});
   }
 
   render() {
+    let  ArrayData = this.state.ArrayData; 
     return(
-    <Row>
+    <Row className="well_box">
     {
-      this.getimgdate()
-    }</Row>
+      <Col span={24} className="wells">
+      <Col span={6}>back</Col>
+      {
+        ArrayData.map(item => (  
+          <Col className="Position_R" 
+                ref={(div)=>{this[`btn-${item}`] = div}}
+               onMouseOver={()=>this.onMouseOver(item)}
+               onMouseOut = {()=>this.onMouseOut(item)}
+                style={{height:this.state.height,backgroundImage:`url(${img})`,backgroundSize:"100% 100%"}} span={6} key={item}>
+            <div className="ShadeWell">{item}</div>
+          </Col>
+        ))
+      }
+      <Col span={6}>next</Col>
+    </Col>
+    }
+    </Row>
     );
   }
 }
