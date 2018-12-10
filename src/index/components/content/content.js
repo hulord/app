e1dba8dcb14e8b8b93ae2ef0323ada8c225ca92c
os.hupsh <os.hupsh@ffcs.cn>
2018-12-10 17:36:52 +0800

    1.提交

--- src/index/components/content/content.js
+++ src/index/components/content/content.js
@@ -1,98 +1,67 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+
 import moment from 'moment';
-import {Row} from 'antd';
+import {Row,Input,Card, Icon, Avatar,Col} from 'antd';
 import 'moment/locale/zh-cn';
 import "./content.less";
 import "../../index.less";
 
 moment.locale('zh-cn');
+
+
+const Search = Input.Search;
+const { Meta } = Card;
 /**
  * 自定义内容主键
  */
-class content extends React.Component {
+class Content extends React.Component {
   constructor(props){
     super( props )
     this.state = {
-      width: props.width || -1,
-      height: props.height || -1,
-      ArrayData:[],
-      hover:false,
-      onMouseOverId:'',
-      content:{
-        top:{
-          title:"",
-          content:"",
-          imgurl:""
-        },
-        today:[
-          {title:"",content:""},
-          {title:"",content:""}
-        ]
-      }
+
     }
   }
-  //组件渲染后调用
-  componentDidMount() {
-    this.updateSize();
-    this.getimgdate();
-    window.addEventListener('resize', () => this.updateSize());
-  }
-  //组件渲染前
-  componentWillUnmount() {
-    window.removeEventListener('resize', () => this.updateSize());
-  }
-
-  updateSize() {
-    try {
-      const parentDom = ReactDOM.findDOMNode(this).parentNode;
-      let { width, height } = this.props;
-      //如果props没有指定height和width就自适应
-      if (!width) {
-        width = parentDom.offsetWidth;
-      }
-      if (!height) {
-        height = width *0.38;
-      }
-      this.setState({ width, height });
-    } catch (ignore) {
-    }
-  }
-  //鼠标悬停事件
-  onMouseOver = (id) =>{
-    //let obj = ReactDOM.findDOMNode(this[`btn-${id}`]);
-    // console.log(this.refs[`btn-`+id+`-content`]);
-    this.setState({onMouseOverId:id});
-  }
-  //鼠标移出事件
-   onMouseOut = (id) =>{
-    this.setState({onMouseOverId:''});
-;
-  }
-  //鼠标点击事件
-  onClick = () =>{
-
-  }
-  getimgdate = ()=>{
-    //获取之前14天的记录
-    //初始时间到目标时间的时间数组
-    let ArrayData = [];
-    for(var i=1;i<15;i++){
-      let CurrentDay = moment().subtract('days',i).date();
-      ArrayData.push(CurrentDay);
-    }
-    ArrayData.sort(function(a,b){return a-b;})
-    this.setState({ArrayData});
-  }
-
   render() {
     return(
       <Row className="content_box">
+          <Search
+            className="content-search"
+            placeholder="input search text"
+            onSearch={value => console.log(value)}
+            enterButton
+          />
+          <Row className="content-list" gutter={10}>
+              <Col span={6}>
+                  <Card
+                    cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+                    actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}
+                  >
+                    <Meta
+                      avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                      title="Card title"
+                      description="This is the description"
+                    />
+                  </Card>
+              </Col>
+              <Col span={6}>
+                  <Card
+                    cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+                    actions={[<Icon type="setting" />, <Icon type="edit" />, <Icon type="ellipsis" />]}
+                  >
+                    <Meta
+                      avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+                      title="Card title"
+                      description="This is the description"
+                    />
+                  </Card>
+              </Col>
 
+
+          </Row>
       </Row>
     );
   }
 }
 
-export default content;
+export default Content;
 
--- src/index/components/layout/Layout.js
+++ src/index/components/layout/Layout.js
@@ -2,9 +2,10 @@ import {Layout, Menu, Icon,Col,Row } from 'antd';
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
 import './layout.less';
+import Content from '../content/content.js'; 
 
 
-const {  Sider, Content } = Layout;
+const {  Sider } = Layout;
 
 
 class SiderDemo extends Component {
@@ -28,35 +29,39 @@ class SiderDemo extends Component {
                     <Col span={18} style={{height:"17vw" }}>FLASH</Col>
                     <Col span={6}>weather</Col>
               </Col>
-                <Col span={24}div style={{height:"100v"}}>
-                  <Col span ={4}>
-                        <Sider
-                          breakpoint="xl"
-                          collapsedWidth="100"
-                          onBreakpoint={(broken) => { console.log(broken); }}
-                          onCollapse={(collapsed, type) => { console.log(collapsed, type); }}
-                        >
-                          <div className="logo" />
-                          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
-                            <Menu.Item key="1">
-                              <Icon type="user" />
-                              <span>nav 1</span>
-                            </Menu.Item>
-                            <Menu.Item key="2">
-                              <Icon type="video-camera" />
-                              <span>nav 2</span>
-                            </Menu.Item>
-                            <Menu.Item key="3">
-                              <Icon type="upload" />
-                              <span>nav 3</span>
-                            </Menu.Item>
-                          </Menu>
-                        </Sider>
+                <Col  className="index-content" span={24} style={{height:"100v"}}>
+                    <Col span ={4}>
+                    <Sider
+                        breakpoint="lg"
+                        collapsedWidth="0"
+                        onBreakpoint={(broken) => { console.log(broken); }}
+                        onCollapse={(collapsed, type) => { console.log(collapsed, type); }}
+                      >
+                        <div className="logo" />
+                        <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+                          <Menu.Item key="1">
+                            <Icon type="user" />
+                            <span className="nav-text">nav 1</span>
+                          </Menu.Item>
+                          <Menu.Item key="2">
+                            <Icon type="video-camera" />
+                            <span className="nav-text">nav 2</span>
+                          </Menu.Item>
+                          <Menu.Item key="3">
+                            <Icon type="upload" />
+                            <span className="nav-text">nav 3</span>
+                          </Menu.Item>
+                          <Menu.Item key="4">
+                            <Icon type="user" />
+                            <span className="nav-text">nav 4</span>
+                          </Menu.Item>
+                        </Menu>
+                      </Sider>
                     </Col>
                     <Col span={20}>
                         <Content style={{ margin: '0 16px' }}>
                           <div style={{ background: '#fff', minHeight: 360,height:'100%' }}>
-                           
+
                           </div>
                         </Content>
                     </Col>
